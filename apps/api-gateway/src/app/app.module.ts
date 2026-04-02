634b1eec188685a3e139a03ae99dd01a4ab908cb
Saptak Banerjee <saptak@pitangent.com>
2026-04-02 18:00:00 +0530

    feat: controller imported and config changes

--- apps/api-gateway/src/app/app.module.ts
+++ apps/api-gateway/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthController } from '../core/auth.controller';
+import { ProxyService } from '../core/proxy.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, AuthController],
+  providers: [AppService, ProxyService],
 })
 export class AppModule {}
